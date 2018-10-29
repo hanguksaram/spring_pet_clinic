6dc0ea92501ddc54050190af99ae63fcc523372c
hanguksaram <artur.pozhidaev@gmail.com>
2018-10-29 10:51:32 +0700

    owner controller and view. closes #14

---
+++ pet_clinic_web/src/main/java/com/scalaboy/spring_pet_clinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.scalaboy.spring_pet_clinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String index() {
+        return "owners/index";
+    }
+}
