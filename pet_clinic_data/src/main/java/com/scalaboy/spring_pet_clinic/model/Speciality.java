7e4c9a4d4229e5548b6d1894aaf793f9264539b3
hanguksaram <artur.pozhidaev@gmail.com>
2018-10-30 12:56:13 +0700

    Vet speciality entity added. closes #19

---
+++ pet_clinic_data/src/main/java/com/scalaboy/spring_pet_clinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.scalaboy.spring_pet_clinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String descriprion;
+
+    public String getDescriprion() {
+        return descriprion;
+    }
+
+    public void setDescriprion(String descriprion) {
+        this.descriprion = descriprion;
+    }
+}
