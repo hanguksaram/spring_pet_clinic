c0c6a44abf3f2a8b0624218ff8a8eb99b06d7a61
hanguksaram <artur.pozhidaev@gmail.com>
2018-11-01 12:13:40 +0700

    junit 5 added. closes #4

---
+++ pet_clinic_web/src/test/java/com/scalaboy/spring_pet_clinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scalaboy.spring_pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class SpringPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
